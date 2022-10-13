36fd6f45338b973335297b030f57e20c0d6577dd
Dennis <denchyk228@gmail.com>
2022-10-13 15:16:39 +0300

    Added chart with live data

--- script.js
+++ script.js
@@ -5,6 +5,9 @@
 let map, heatmap;
 let planesByFlag = {};
 let countryCodes;
+let chart;
+Chart.defaults.font.size = 14;
+
 const flagsSelect = document.getElementById('select-planes');
 let gradient = [
     "rgba(142, 114, 194, 0.1)",
@@ -109,30 +112,13 @@ const createHeatmap = (data) => {
             gradient: gradient
         });
         updatePlanesSelect();
+        loadChart();
     }
 }
 
 const updatePlanesSelect = () => {
     let flags = Object.keys(planesByFlag);
     flags.sort((flag_a, flag_b) => planesByFlag[flag_b] - planesByFlag[flag_a]);
-
-    // fetch('https://airlabs.co/api/v9/countries?api_key=b2b4091f-292e-4cb2-b92c-005161695af3')
-    //   .then(response => response.json())
-    //   .then(data => {
-    //     let countriesCodes: Array<any> = data.response;
-    //     for (const flagCode of flags) {
-    //       let country = countriesCodes.filter(country => country.code == flagCode)[0];
-
-    //       if (country) {
-    //         flagsSelect.appendChild(new Option(`${country.name} - ${planesByFlag[flagCode]}`, flagCode));
-    //       } else {
-    //         flagsSelect.appendChild(new Option(`${flagCode} - ${planesByFlag[flagCode]}`, flagCode));
-    //       }
-    //     }
-    //   })
-    //   .catch(e => {
-    //     console.log(e);
-    //   })
 
     for (const flagCode of flags) {
         let country = countryCodes.filter(country => country.code == flagCode)[0];
@@ -159,39 +145,112 @@ flagsSelect.addEventListener('change', (event) => {
 window.initMap = initMap;
 
 
-const labels = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-];
+const loadChart = () => {
 
-const data = {
-    labels: labels,
-    datasets: [{
-        label: 'My First dataset',
-        backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgb(255, 99, 132)',
-        data: [0, 10, 5, 2, 20, 30, 45],
-    }]
-};
+    let width = window.screen.width;
+    let showOptions = 7;
 
-const config = {
-    type: 'line',
-    data: data,
-    options: {}
-};
+    if (width <= 768) {
+        showOptions = 6;
+    }
 
+    let allFlags = Object.keys(planesByFlag);
+    allFlags.sort((flag_a, flag_b) => planesByFlag[flag_b] - planesByFlag[flag_a]);
+    const flags = allFlags.slice(0, showOptions);
 
-window.addEventListener('DOMContentLoaded', () => {
-    const myChart = new Chart(
+    let labels = flags.map(flagCode => countryCodes.filter(country => country.code == flagCode)[0].name);
+    let planeNnumbers = flags.map(flagCode => planesByFlag[flagCode])
+
+    const data = {
+        labels: labels,
+        datasets: [{
+            axis: 'y',
+            label: 'Planes by the flag',
+            backgroundColor: 'rgba(66, 25, 148, 1)',
+            data: planeNnumbers,
+            inflateAmount: -3,
+            borderRadius: 3,
+            hoverBackgroundColor: 'rgba(109, 56, 214, 0.8)'
+
+        }]
+    };
+
+    const config = {
+        type: 'bar',
+        data: data,
+        plugins: [ChartDeferred],
+        options: {
+            indexAxis: 'y',
+            plugins: {
+                deferred: {
+                    xOffset: 150,   // defer until 150px of the canvas width are inside the viewport
+                    yOffset: '50%', // defer until 50% of the canvas height are inside the viewport
+                    // delay: 0      // delay of 500 ms after the canvas is considered inside the viewport
+                }
+            },
+            transitions: {
+                show: {
+                    animations: {
+                        x: {
+                            from: 0
+                        }
+                    }
+                },
+                hide: {
+                    animations: {
+                        x: {
+                            to: 0
+                        },
+                    }
+                },
+                // resize: {
+                //     animations: {
+                //         x: {
+                //             from: 0
+                //         }
+                //     }
+                // }
+            },
+            animations: {
+                y: { duration: 0 }
+            },
+
+        }
+    };
+
+    // if (chart) chart.destroy();
+
+    chart = new Chart(
         document.getElementById('planeByCountriesChart'),
         config
     );
-})
+}
+
+const updateLegend = () => {
+    let width = window.innerWidth;
+    // console.log(width);
+    let showOptions = 7;
+
+    if (width <= 672) {
+        showOptions = 5;
+    }
+    else if (width <= 768) {
+        showOptions = 6;
+    }
+
+    let allFlags = Object.keys(planesByFlag);
+    allFlags.sort((flag_a, flag_b) => planesByFlag[flag_b] - planesByFlag[flag_a]);
+    const flags = allFlags.slice(0, showOptions);
+
+    let labels = flags.map(flagCode => countryCodes.filter(country => country.code == flagCode)[0].name);
+    let planeNnumbers = flags.map(flagCode => planesByFlag[flagCode])
+
+    chart.data.datasets.data = planeNnumbers;
+    chart.data.labels = labels;
+    chart.update();
+}
 
 
 
 
+window.addEventListener('resize', updateLegend);
