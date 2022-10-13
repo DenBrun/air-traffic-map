2fd3044cac3212670eb820f7e7d60b50d41bf796
Dennis <denchyk228@gmail.com>
2022-10-13 13:31:37 +0300

    Migrated to js file

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // This example requires the Visualization library. Include the libraries=visualization
 // parameter when you first load the API. For example:
 // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=visualization">
+
 let map, heatmap;
 let planesByFlag = {};
 let countryCodes;
@@ -22,15 +23,22 @@ let gradient = [
     "rgba(216, 74, 88, 1)",
     "rgba(223, 25, 25, 1)",
     "rgba(223, 25, 25, 1)",
+
 ];
+
 // setTimeout(() => {
 // fetch('./data/country-codes.json')
 //   .then((response) => response.json())
 //   .then((json) => countryCodes = json);
 // }, 2000)
+
+
+
 fetch('./data/country-codes.json')
     .then((response) => response.json())
     .then((json) => countryCodes = json);
+
+
 function initMap() {
     console.log("Archibaldo Eduardo2");
     map = new google.maps.Map(document.getElementById("map"), {
@@ -38,54 +46,62 @@ function initMap() {
         center: { lat: 33.749374, lng: -26.451329 },
         mapId: "abdf9465a5f0e4c7",
     });
+
     map.addListener("maptypeid_changed", () => {
         if (map.getMapTypeId() !== "roadmap") {
             heatmap.set("gradient", null);
-        }
-        else {
+        } else {
             heatmap.set("gradient", gradient);
         }
-    });
+    })
+
+
     getPoints();
     console.log(planesByFlag);
+
 }
+
+
+
+
 function getPoints(specialFlag = null) {
     let url;
     if (specialFlag && specialFlag != 'all') {
         url = `https://airlabs.co/api/v9/flights?api_key=b2b4091f-292e-4cb2-b92c-005161695af3&flag=${specialFlag}`;
-    }
-    else {
+    } else {
         url = "https://airlabs.co/api/v9/flights?api_key=b2b4091f-292e-4cb2-b92c-005161695af3";
     }
+
+
     fetch(url.toString())
         .then(response => response.json())
         .then(data => {
-        createHeatmap(data);
-    })
+            createHeatmap(data);
+        })
         .catch(e => {
-        console.log(e);
-    });
+            console.log(e);
+        })
 }
+
 const createHeatmap = (data) => {
     let points = [];
     for (const plane of data.response) {
         points.push(new google.maps.LatLng(plane.lat, plane.lng));
         if (plane.flag) {
             if (plane.flag == "UK" || plane.flag == "GB") {
-                plane.flag = "GB&flag=UK";
+                plane.flag = "GB&flag=UK"
             }
             if (plane.flag in planesByFlag) {
                 planesByFlag[plane.flag] += 1;
-            }
-            else {
+            } else {
                 planesByFlag[plane.flag] = 1;
             }
         }
     }
+
     if (heatmap) {
         heatmap.setData(points);
-    }
-    else {
+    } else {
         heatmap = new google.maps.visualization.HeatmapLayer({
             data: points,
             map: map,
@@ -94,16 +110,19 @@ const createHeatmap = (data) => {
         });
         updatePlanesSelect();
     }
-};
+}
+
 const updatePlanesSelect = () => {
     let flags = Object.keys(planesByFlag);
     flags.sort((flag_a, flag_b) => planesByFlag[flag_b] - planesByFlag[flag_a]);
+
     // fetch('https://airlabs.co/api/v9/countries?api_key=b2b4091f-292e-4cb2-b92c-005161695af3')
     //   .then(response => response.json())
     //   .then(data => {
     //     let countriesCodes: Array<any> = data.response;
     //     for (const flagCode of flags) {
     //       let country = countriesCodes.filter(country => country.code == flagCode)[0];
+
     //       if (country) {
     //         flagsSelect.appendChild(new Option(`${country.name} - ${planesByFlag[flagCode]}`, flagCode));
     //       } else {
@@ -114,22 +133,32 @@ const updatePlanesSelect = () => {
     //   .catch(e => {
     //     console.log(e);
     //   })
+
     for (const flagCode of flags) {
         let country = countryCodes.filter(country => country.code == flagCode)[0];
+
         if (country) {
             flagsSelect.appendChild(new Option(`${country.name} - ${planesByFlag[flagCode]}`, flagCode));
-        }
-        else {
+        } else {
             flagsSelect.appendChild(new Option(`${flagCode} - ${planesByFlag[flagCode]}`, flagCode));
         }
     }
-};
-// TODO: Fix UK and GB
+
+}
+
+
+
+
 flagsSelect.addEventListener('change', (event) => {
     let select = event.target;
     getPoints(select.value);
-});
+})
+
+
+
 window.initMap = initMap;
+
+
 const labels = [
     'January',
     'February',
@@ -138,20 +167,31 @@ const labels = [
     'May',
     'June',
 ];
+
 const data = {
     labels: labels,
     datasets: [{
-            label: 'My First dataset',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45],
-        }]
+        label: 'My First dataset',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: [0, 10, 5, 2, 20, 30, 45],
+    }]
 };
+
 const config = {
     type: 'line',
     data: data,
     options: {}
 };
+
+
 window.addEventListener('DOMContentLoaded', () => {
-    const myChart = new Chart(document.getElementById('planeByCountriesChart'), config);
-});
+    const myChart = new Chart(
+        document.getElementById('planeByCountriesChart'),
+        config
+    );
+})
+
+
+
+
